e6060a343826f5755cdccbd6b5d13f228cd86125
Greatoxy1 <tahirus3456789@gmail.com>
2025-05-23 18:45:44 +0200

    React Session 01: Message board setup

---
+++ src/components/Post.jsx
@@ -0,0 +1,18 @@
+import './Post.css';
+
+export function Post() {
+  return (
+    <article className="Post">
+      <h2 className="Post-title">My first Posting</h2>
+      <div className=" Post-content">
+        <span className="post-author">from Tahiru</span>
+        <span className="Post-date">
+          on <time>2025-05-11</time>
+        </span>
+        <div className="Post-button">
+          <button>Read more</button>
+        </div>
+      </div>
+    </article>
+  );
+}
